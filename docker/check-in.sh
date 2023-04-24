58357771ba8f5d260485f6834268897f78a7cb3c
Timo Steinbring <timo.steinbring@cloos.de>
2023-04-24 08:16:53 +0200

    [docker] added docker build

---
+++ docker/check-in.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -e
+
+echo "APP_PLATFORM_URL: ${APP_PLATFORM_URL}"
+echo "APP_ID:           ${APP_ID}"
+echo "APP_TOKEN:        ${APP_TOKEN}"
+echo "APP_FRONTEND_URL: ${APP_FRONTEND_URL}"
+echo ""
+
+echo "starting nginx..."
+nginx
+
+while [ true ];  
+do
+  curl --silent --request PUT \
+  --url "${APP_PLATFORM_URL}/api/applicationmanagement/apps/${APP_ID}" \
+  --header 'Content-Type: application/json' \
+  --data "{
+            \"token\": \"${APP_TOKEN}\",
+            \"backendUrl\": \"\",
+            \"frontendUrl\": \"${APP_FRONTEND_URL}\"
+          }"
+
+  if [[ "$?" -eq 0 ]]; then
+    echo "Check-in was successful"
+  else 
+    echo "Check-in failed"
+  fi
+
+  sleep 60;
+  
+done
