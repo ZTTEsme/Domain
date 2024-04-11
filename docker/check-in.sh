59e811b9c9cb32c77eee676b10a3d54282238e10
Timo Steinbring <timo.steinbring@cloos.de>
2024-04-11 10:56:49 +0200

    upgraded SDK, Dockerfile and build script

--- docker/check-in.sh
+++ docker/check-in.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -e
 
-echo "APP_PLATFORM_URL: ${APP_PLATFORM_URL}"
-echo "APP_ID:           ${APP_ID}"
-echo "APP_TOKEN:        ${APP_TOKEN}"
-echo "APP_FRONTEND_URL: ${APP_FRONTEND_URL}"
+echo "APP_APPLICATION_MANAGEMENT_SERVICE_URL:  ${APP_APPLICATION_MANAGEMENT_SERVICE_URL}"
+echo "APP_APP_ID:                              ${APP_APP_ID}"
+echo "APP_FRONTEND_URL:                        ${APP_FRONTEND_URL}"
+echo "APP_AUTH_TOKEN_URL:                      ${APP_AUTH_TOKEN_URL}"
+echo "APP_AUTH_TOKEN_CLIENT_ID:                ${APP_AUTH_TOKEN_CLIENT_ID}"
+echo "APP_AUTH_TOKEN_CLIENT_SECRET:            ${APP_AUTH_TOKEN_CLIENT_SECRET}"
 echo ""
 
 echo "starting nginx..."
@@ -11,11 +13,13 @@ nginx
 
 while [ true ];  
 do
+  accessToken=$(curl --silent -d "client_secret=${APP_AUTH_TOKEN_CLIENT_SECRET}&grant_type=client_credentials&client_id=${APP_AUTH_TOKEN_CLIENT_ID}" -H "Content-Type: application/x-www-form-urlencoded" -X POST ${APP_AUTH_TOKEN_URL} | jq --raw-output .access_token )
+
   curl --silent --request PUT \
-  --url "${APP_PLATFORM_URL}/api/applicationmanagement/apps/${APP_ID}" \
-  --header 'Content-Type: application/json' \
+  --url "${APP_APPLICATION_MANAGEMENT_SERVICE_URL}/api/application-management/apps/${APP_APP_ID}" \
+  --header "content-type: application/json" \
+  --header "authorization: ${accessToken}" \
   --data "{
-            \"token\": \"${APP_TOKEN}\",
             \"backendUrl\": \"\",
             \"frontendUrl\": \"${APP_FRONTEND_URL}\",
             \"entryPoint\": \"#!/\",
